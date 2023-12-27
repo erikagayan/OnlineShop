4b98f18b1d5d6844812b15d0138fa8e3e29f66b7
Erik Agayan <erik.agayan1@gmail.com>
2023-12-27 16:04:48 +0200

    Add manager and moderator

--- users/admin.py
+++ users/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from users.models import User
 
+
+class UserAdmin(UserAdmin):
+    list_display = ("username", "email", "is_moderator", "is_manager")
+    fieldsets = (
+        (None, {"fields": ("username", "email", "password")}),
+        ("Permissions", {"fields": ("is_moderator", "is_manager")}),
+    )
+
 admin.site.register(User, UserAdmin)
--- users/models.py
+++ users/models.py
@@ -3,4 +3,5 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    is_moderator = models.BooleanField(default=False)
+    is_manager = models.BooleanField(default=False)
