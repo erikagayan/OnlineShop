9cbb2f906e56a87f60b161de76d6b12434050d05
Erik Agayan <erik.agayan1@gmail.com>
2023-12-28 15:35:05 +0200

    Add permissions for moderator and change permissions.py for moderator and admin

--- users/admin.py
+++ users/admin.py
@@ -7,7 +7,7 @@ class UserAdmin(UserAdmin):
     list_display = ("username", "email", "is_moderator", "is_manager")
     fieldsets = (
         (None, {"fields": ("username", "email", "password")}),
-        ("Permissions", {"fields": ("is_moderator", "is_manager")}),
+        ("Permissions", {"fields": ("is_moderator", "is_manager", "is_staff", "groups")}),
     )
 
 admin.site.register(User, UserAdmin)
