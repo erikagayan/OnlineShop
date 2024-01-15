9c7ebb769181decd44eb520f94157a762c4d2653
Erik Agayan <erik.agayan1@gmail.com>
2024-01-15 14:41:26 +0200

    Add login via email

--- users/serializers.py
+++ users/serializers.py
@@ -20,10 +20,9 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = get_user_model().objects.create(
-            username=validated_data['username'],
-            email=validated_data['email']
+            username=validated_data["username"], email=validated_data["email"]
         )
-        user.set_password(validated_data['password'])
+        user.set_password(validated_data["password"])
         user.save()
         return user
 
--- users/validators.py
+++ users/validators.py
@@ -5,10 +5,7 @@ from django.contrib.auth.password_validation import validate_password
 class CustomPasswordValidator:
     def validate_password(self, password, user=None):
         try:
-            # Проверка пароля с использованием валидаторов Django
             validate_password(password, user)
         except ValidationError as e:
-            # Возвращаем ошибки валидации
             return list(e.messages)
         return None
-
