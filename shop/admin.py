d5b9ac40cfea25d9e349d2d6dbe955adc6bb1693
Erik <erik.agayan1@gmail.com>
2024-08-29 13:37:04 +0300

    Add users in admin panel

--- shop/admin.py
+++ shop/admin.py
@@ -6,11 +6,30 @@ from django import forms
 import requests
 from .models import MicroserviceUserProxy
 from django.contrib.admin.views.main import ChangeList
+from django.contrib.admin.helpers import AdminForm
 
 class UserEditForm(forms.ModelForm):
     class Meta:
         model = MicroserviceUserProxy
         fields = ['email', 'username', 'is_manager', 'is_moderator']
+
+class UserAddForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput, label='Password')
+    password_confirmation = forms.CharField(widget=forms.PasswordInput, label='Confirm Password')
+
+    class Meta:
+        model = MicroserviceUserProxy
+        fields = ['email', 'username', 'is_manager', 'is_moderator', 'password', 'password_confirmation']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        password_confirmation = cleaned_data.get("password_confirmation")
+
+        if password and password_confirmation and password != password_confirmation:
+            self.add_error('password_confirmation', "Passwords do not match")
+
+        return cleaned_data
 
 class MicroserviceUserQuerySet:
     def __init__(self, data):
@@ -42,16 +61,22 @@ class MicroserviceUserQuerySet:
 
 class MicroserviceUserChangeList(ChangeList):
     def get_queryset(self, request):
-        # Возвращаем пустой QuerySet, чтобы избежать обращения к базе данных
         return MicroserviceUserQuerySet([])
 
 class MicroserviceUserAdmin(admin.ModelAdmin):
     list_display = ('id', 'email', 'username', 'is_manager', 'is_moderator')
     readonly_fields = ('id',)
     actions = ['delete_users']
+    form = UserEditForm
+
+    # Добавляем add_fieldsets для отображения дополнительных полей при добавлении пользователя
+    add_fieldsets = (
+        (None, {
+            'fields': ('email', 'username', 'is_manager', 'is_moderator', 'password', 'password_confirmation'),
+        }),
+    )
 
     def get_changelist(self, request, **kwargs):
-        # Используем кастомный ChangeList
         return MicroserviceUserChangeList
 
     def get_urls(self):
@@ -62,11 +87,9 @@ class MicroserviceUserAdmin(admin.ModelAdmin):
         return custom_urls + urls
 
     def get_queryset(self, request):
-        # Возвращаем пустой QuerySet, чтобы избежать обращения к базе данных
         return MicroserviceUserQuerySet([])
 
     def changelist_view(self, request, extra_context=None):
-        # Используем кастомный ChangeList
         response = super().changelist_view(request, extra_context)
         try:
             token = request.session.get('auth_token') or request.headers.get('Authorization')
@@ -86,7 +109,6 @@ class MicroserviceUserAdmin(admin.ModelAdmin):
 
             users_data = response_data.json()
 
-            # Оборачиваем данные в кастомный QuerySet
             users_queryset = MicroserviceUserQuerySet([
                 MicroserviceUserProxy(
                     id=user['id'],
@@ -97,7 +119,6 @@ class MicroserviceUserAdmin(admin.ModelAdmin):
                 ) for user in users_data
             ])
 
-            # Обновляем контекст для отображения данных в админ-панели
             extra_context = extra_context or {}
             response.context_data['cl'].result_list = users_queryset
             response.context_data['cl'].full_result_count = users_queryset.count()
@@ -108,10 +129,66 @@ class MicroserviceUserAdmin(admin.ModelAdmin):
             self.message_user(request, f"Unexpected error: {e}", level='error')
         return response
 
+    def add_view(self, request, form_url='', extra_context=None):
+        """
+        Custom view to handle the addition of a new user.
+        """
+        if request.method == 'POST':
+            form = UserAddForm(request.POST)
+            if form.is_valid():
+                token = request.session.get('auth_token') or request.headers.get('Authorization')
+                if not token:
+                    self.message_user(request, "Authorization token is missing.", level='error')
+                    return HttpResponseRedirect('..')
+
+                if token.startswith('Bearer '):
+                    token = token.split(' ', 1)[1]
+
+                headers = {'Authorization': f'Bearer {token}'}
+                user_data = form.cleaned_data
+                user_data.pop('password_confirmation')  # Убираем поле подтверждения пароля
+
+                try:
+                    response = requests.post(
+                        f'{settings.MICROSERVICE_USER_URL}/api/users/register/',
+                        headers=headers,
+                        json=user_data
+                    )
+                    if response.status_code in [200, 201]:
+                        self.message_user(request, "User added successfully.")
+                    else:
+                        self.message_user(request, f"Error adding user: {response.status_code}", level='error')
+                except Exception as e:
+                    self.message_user(request, f"Unexpected error: {e}", level='error')
+                return HttpResponseRedirect('..')
+
+        else:
+            form = UserAddForm()
+
+        # Создание объекта AdminForm для рендеринга формы в админ-панели
+        admin_form = AdminForm(form, self.add_fieldsets, self.prepopulated_fields, self.readonly_fields, model_admin=self)
+
+        # Обновляем контекст для метода render_change_form
+        context = {
+            'adminform': admin_form,
+            'form': form,
+            'add': True,
+            'change': False,
+            'is_popup': False,
+            'save_as': False,
+            'has_add_permission': True,
+            'has_change_permission': False,
+            'has_delete_permission': False,
+            'has_view_permission': True,
+            'show_save_and_add_another': False,
+            'show_save_and_continue': False,
+            'inline_admin_formsets': [],  # Обязательно добавляем этот ключ в контекст
+            'errors': form.errors,  # Отображение ошибок формы
+        }
+
+        return self.render_change_form(request, context, add=True, change=False, form_url=form_url)
+
     def edit_user(self, request, user_id):
-        """
-        Custom view for editing a user.
-        """
         if request.method == 'POST':
             form = UserEditForm(request.POST)
             if form.is_valid():
@@ -156,7 +233,7 @@ class MicroserviceUserAdmin(admin.ModelAdmin):
                 if response.status_code == 200:
                     user_data = response.json()
                     form = UserEditForm(initial=user_data)
-                    return self.admin_site.admin_view(render)(request, 'admin/edit_user.html', {'form': form, 'user_id': user_id})
+                    return self.render_change_form(request, context={'form': form, 'user_id': user_id})
                 else:
                     self.message_user(request, f"Error fetching user: {response.status_code}", level='error')
             except Exception as e:
@@ -165,9 +242,6 @@ class MicroserviceUserAdmin(admin.ModelAdmin):
             return HttpResponseRedirect('..')
 
     def delete_users(self, request, queryset):
-        """
-        Action for deleting users via microservice API.
-        """
         for user in queryset:
             token = request.session.get('auth_token') or request.headers.get('Authorization')
             if not token:
@@ -189,6 +263,5 @@ class MicroserviceUserAdmin(admin.ModelAdmin):
                 self.message_user(request, f"Unexpected error: {e}", level='error')
 
     delete_users.short_description = 'Delete selected users'
-
 
 admin.site.register(MicroserviceUserProxy, MicroserviceUserAdmin)
